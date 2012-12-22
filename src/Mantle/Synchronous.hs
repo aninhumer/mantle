f583322931b08dafa579a58697b857e5713743a3
Alex Horsman <aninhumer@gmail.com>
2012-12-22 21:35:24 +0000

    Replaced syncBlock and makeSync with fixed makeSync function.
    SyncComp is added as a dual to Component.

--- src/Mantle/Synchronous.hs
+++ src/Mantle/Synchronous.hs
@@ -18,6 +18,7 @@ import Data.Vector.Bit
 import Mantle.RTL
 import Mantle.Circuit
 import Mantle.Logic
+import Mantle.Interface
 
 newtype Clock = Clock (Wire Bool)
 
@@ -33,26 +34,22 @@ type ClockReset = (Clock,Reset)
 
 type Synchronous = ReaderT ClockReset Circuit
 
+type SyncComp ifc a = Inner ifc -> Synchronous a
 
 instance MonadCircuit Synchronous where
     liftCircuit = lift
 
+makeSync :: Interface ifc =>
+    ClockReset -> SyncComp ifc a -> Circuit (Outer ifc)
+makeSync cr syncF = (`runReaderT` cr) $ do
+    ifc <- newIfc
+    syncF ifc
+    return $ expose ifc
 
 onSync :: Clock -> Reset -> Trigger
 onSync (Clock c) (Reset r) = Set.fromList [PosEdge c, NegEdge r]
 
-syncBlock :: Clock -> Reset -> Sync -> Statement
-syncBlock (Clock c) (Reset r) (Sync is us) = Cond noReset updates initial
-  where
-    noReset = Var r
-    updates = S.fromList $ map (uncurry AsyncAssign) us
-    initial = S.fromList $ map (uncurry AsyncAssign . second Lit) is
 
-makeSync :: Clock -> Reset -> Synchronous a -> Circuit a
-makeSync clk rst s = do
-    (out,syn) <- runWriterT s
-    addStmt (onSync clk rst) $ syncBlock clk rst syn
-    return out
 
 (<=:) :: Reg a -> Logic a -> Synchronous ()
 (Reg r) <=: (Logic e) = do
