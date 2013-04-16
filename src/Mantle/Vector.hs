a7f9866002a55ce2ed589ffa1601eaa04db017af
Alex Horsman <aninhumer@gmail.com>
2013-04-16 11:52:42 +0100

    Added functions for creating vectors of external IO.

--- src/Mantle/Vector.hs
+++ src/Mantle/Vector.hs
@@ -1,27 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE DataKinds #-}
 
 module Mantle.Vector where
 
 import qualified Data.Vector.Fixed as V
-import Data.Vector.Fixed.Boxed (Vec)
+import qualified Data.Vector.Fixed.Boxed as VB
 
 import Mantle.Bits
 import Mantle.RTL
 import Mantle.Circuit
+import Mantle.Logic
 import Mantle.Interface
 
-type VectorIfc n a d = Vec n (Signal a d)
+type Vec = VB.Vec
 
-instance (V.Arity n, Direction d, Bits a) =>
-    Interface (VectorIfc n a d) where
-    type FlipIfc (VectorIfc n a d) = VectorIfc n a (Flip d)
+instance (V.Arity n, Interface a) =>
+    Interface (Vec n a) where
+    type FlipIfc (Vec n a) = Vec n (FlipIfc a)
     newIfc = do
         pairs <- V.replicateM newIfc
         return (V.map fst pairs, V.map snd pairs)
@@ -29,10 +25,32 @@ instance (V.Arity n, Direction d, Bits a) =>
         V.zipWithM (=:) xs ys
         return ()
 
-type Vector n a = VectorIfc n a Outer
+extInputVec :: forall mc n a . (MonadCircuit mc, V.Arity n, Bits a)
+    => String -> mc (Vec n (Output a))
+extInputVec name = do
+    (ExtInput x :: ExtInput a) <- newExtInput name
+    return $ V.generate $ pos x
+  where
+    pos x i = index (literal i) (Output (Var x))
+
+extOutputVec :: forall mc n a. (MonadCircuit mc, V.Arity n, Bits a)
+    => String -> mc (Vec n (Input a))
+extOutputVec name = do
+    (ExtOutput x :: ExtOutput a) <- newExtOutput name
+    return $ V.generate $ pos x
+  where
+    pos x i = indexedInput x (Lit $ Dec $ toInteger i)
+
+-- TODO: Better constraints on i vs n
+index :: Bits i => Output i -> Output (Vec n a) -> Output a
+index (Output e) (Output (Var x)) =
+    Output $ Index x e
+
+(!) :: Bits i => Output (Vec n a) -> Output i -> Output a
+(!) = flip index
 
 
 type Memory n a = Reg (Vec n a)
 
-index :: Bits i => Output i -> Memory n a -> Reg a
-index (Output e) (Reg (NormalRef m)) = Reg $ IndexedRef m e
+indexMem :: Bits i => Output i -> Memory n a -> Reg a
+indexMem (Output e) (Reg (NormalRef m)) = Reg $ IndexedRef m e
